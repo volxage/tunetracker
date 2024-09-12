686fd3aabc0152e4d758c10d6062ffef00d9b1ee
volxage <jhilla@jhilla.org>
2024-09-12 19:30:04 -0400

    Add writers to Composer model

--- model/Composer.js
+++ model/Composer.js
@@ -1,6 +1,7 @@
 //Copyright 2024 Jonathan Hilliard
 import { Model } from '@nozbe/watermelondb'
 import { text, lazy, date, field} from '@nozbe/watermelondb/decorators'
+import {composerDefaults} from '../types';
 
 export default class Composer extends Model {
   static table = 'composers';
@@ -9,6 +10,32 @@ export default class Composer extends Model {
     tune_composers: { type: 'has_many', foreignKey: 'composer_id' }
   }
 
+  @writer async changeAttr(attr, newValue){
+    //TODO: Validate type. Import prettyAttrs?
+    await this.update(composer => {
+      composer[attr] = newValue;
+    });
+  }
+  @writer async replace(newComposer){
+    await this.update(composer => {
+      for(let attrPair of composerDefaults){
+        if(attrPair[0] in newComposer){
+          console.log(attrPair[0]);
+          composer[attrPair[0]] = newComposer[attrPair[0]];
+        }
+      }
+    })
+    console.log(this.dbId);
+  }
+  *attrs(start = 0, end = Infinity, step = 1){
+    for(let attrPair of composerDefaults){
+      if(attrPair[0] in this){
+        yield this[attrPair[0]];
+      }else{
+        yield attrPair[1];
+      }
+    }
+  }
   @lazy
   tunes = this.collections
     .get('tunes')
