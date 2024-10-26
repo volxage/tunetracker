b48e9160cd4c76106ca3c75f02d34a6cfc034625
volxage <jhilla@jhilla.org>
2024-10-26 12:28:06 -0400

    Add state to be used by PlaylistImporter

--- components/PlaylistImporter.tsx
+++ components/PlaylistImporter.tsx
@@ -8,16 +8,30 @@ import Tune from "../model/Tune";
 import {List, Results} from "realm";
 import RNFS from "react-native-fs";
 import Share from "react-native-share";
-import RNDP from "react-native-document-picker";
+import RNDP, {DocumentPickerResponse} from "react-native-document-picker";
 import buff from "buffer";
+import {tune_draft} from "../types";
 const tmpPlaylistPath = RNFS.TemporaryDirectoryPath + "/tmp_playlist.json";
 
 
 export default function PlaylistImporter({navigation}: {navigation: any}){
-  const [importedPlaylist, setImportedPlaylist] = useState([]);
+  const [importedPlaylist, setImportedPlaylist]: [tune_draft[], Function] = useState([]);
+  const [commonSongs, setCommonSongs]: [Tune[], Function] = useState([]);
+  const [unlearnedSongs, setUnlearnedSongs]: [tune_draft[], Function] = useState([]);
   return (
     <View>
-      <Text>{JSON.stringify(importedPlaylist)}</Text>
+      <Text>Common Songs</Text>
+      {
+        commonSongs.map(song => 
+          <SubText>{song.title} by {song.composers?.map(comp => comp.name).join(", ")}</SubText>
+        )
+      }
+      <Text>Songs you don't know</Text>
+      {
+        unlearnedSongs.map(tn => {
+          return(<SubText>{tn.title}</SubText>);
+        })
+      }
       <Button
         onPress={async () => {
           RNDP.pick({
@@ -25,9 +39,18 @@ export default function PlaylistImporter({navigation}: {navigation: any}){
           }).catch(err => {
             console.error(err);
           }).then(result => {
+            if(result && result.length > 0){
+              RNFS.readFile(result[0].uri, "base64").then(res => {
+                //TODO:
+                // Find all local songs with dbIds that exist in this list.
+                // Place thenm in commonSongs
+                //
+                // Take other songs and put them in unlearnedSongs, consider a type that uses composer_draft[] instead of Composer[]
+                setUnlearnedSongs(JSON.parse(new buff.Buffer(res, 'base64').toString('utf8')));
+              });
+            }
             console.log(result);
-          }
-          )
+          })
         }}
       >
         <ButtonText>Try opening</ButtonText>
