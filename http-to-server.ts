63e175f426ed06c28d6da3efe0fa40a345b86d62
volxage <jhilla@jhilla.org>
2024-12-19 21:27:37 -0500

    Add withCredentials to Axios

--- http-to-server.ts
+++ http-to-server.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
+  withCredentials: true,
   baseURL: "https://api.jhilla.org:443/tunetracker",
   headers: {
     "Content-type": "application/json",
