a1e904e7df43efa595f45576915a922fcc8f1414
volxage <jhilla@jhilla.org>
2025-06-17 19:17:24 -0400

    Add missing UploadRequest screen to Editor

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -38,6 +38,7 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import {useTheme} from 'styled-components';
 import SimilarItemPrompt from './connection-management/SimilarItemPrompt.tsx';
 import ConfirmConectionPrompt from './connection-management/ConfirmConnectionPrompt.tsx';
+import UploadRequest from './connection-management/UploadRequest.tsx';
 
 
 //Wish there was a better way of doing this
@@ -306,6 +307,7 @@ export default function Editor({
         }
       </Stack.Screen>
       <Stack.Screen name='SimilarItemPrompt' component={SimilarItemPrompt}/>
+      <Stack.Screen name='UploadRequest' component={UploadRequest}/>
       <Stack.Screen name='ConfirmConnectionPrompt' component={ConfirmConectionPrompt}/>
     </Stack.Navigator>
 </TuneDraftContext.Provider>
