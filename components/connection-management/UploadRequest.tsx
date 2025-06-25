43b49c93be24dd7efb82755ca55550d8e4da3377
volxage <jhilla@jhilla.org>
2025-06-25 15:37:08 -0400

    Some translations for UploadRequest (So it's more like Compare)

--- components/connection-management/UploadRequest.tsx
+++ components/connection-management/UploadRequest.tsx
@@ -3,9 +3,15 @@ import {ButtonText, DeleteButton, SafeBgView, SMarginView, SubText, Text, Title}
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import {Button} from "../../simple_components/Button";
 import {useNavigation} from "@react-navigation/native";
-import {useContext, useState} from "react";
+import {useContext, useEffect, useReducer, useState} from "react";
 import TuneDraftContext from "../../contexts/TuneDraftContext";
 import ComposerDraftContext from "../../contexts/ComposerDraftContext";
+import standardComposerDraftReducer from "../../DraftReducers/StandardComposerDraftReducer";
+import standardTuneDraftReducer from "../../DraftReducers/StandardTuneDraftReducer";
+import ResponseHandler from '../services/ResponseHandler.ts';
+import {AxiosError} from "axios";
+import OnlineDB from "../../OnlineDB";
+import {standard_composer, standard_draft} from "../../types";
 
 export default function UploadRequest({}: {}){
   //TODO: Translate tune to standard, fix submit button, hide cancel button if success
@@ -19,10 +25,17 @@ export default function UploadRequest({}: {}){
   const CDContext = useContext(ComposerDraftContext);
   //If the composerDraftContext isn't empty.
   const isComposer = Object.keys(CDContext).length > 0;
+  const [dbState, dbDispatch] = useReducer(
+    (isComposer ? standardComposerDraftReducer : standardTuneDraftReducer), {currentDraft: {}}
+  );
+  const convertedStd = dbState.currentDraft;
+  const onlineDbDispatch = useContext(OnlineDB.DbDispatchContext);
+  useEffect(() => {
+  })
   function submit(first=true){
     if(!uploadSuccessful && !errorReceived){
       if(!isComposer){
-        const toUpload = comparedDbChanges as standard_draft;
+        const toUpload = convertedStd as standard_draft;
         const copyToSend = {
           title: toUpload.title,
           alternative_title: toUpload.alternative_title,
@@ -44,10 +57,9 @@ export default function UploadRequest({}: {}){
         ).then(res => {
           setUploadResult(res.result);
           setUploadErrorPresent(res.isError);
-          handleSetCurrentItem("dbDraftId", res.data.data.id, true);
         })
       }else{
-        const toUpload = comparedDbChanges as standard_composer;
+        const toUpload = convertedStd as standard_composer;
         const copyToSend = {
           name: toUpload.name,
           bio: toUpload.bio,
@@ -67,7 +79,6 @@ export default function UploadRequest({}: {}){
         ).then(res => {
           setUploadResult(res.result);
           setUploadErrorPresent(res.isError);
-          handleSetCurrentItem("dbDraftId", res.data.data.id, true);
         })
       }
     }
