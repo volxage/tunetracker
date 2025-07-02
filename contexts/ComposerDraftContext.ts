45678b46cbb65ce56df427128d2ccbd9fba79be9
volxage <jhilla@jhilla.org>
2025-07-02 10:03:30 -0400

    Export type of tune/composer_draft_context_t

--- contexts/ComposerDraftContext.ts
+++ contexts/ComposerDraftContext.ts
@@ -1,11 +1,11 @@
 import {createContext} from "react"
 import {composer} from "../types";
-type composer_draft_struct = {
+export type composer_draft_context_t = {
   cd: composer,
   setCd: (cd: composer) => void,
   //There's a technical difference between optional parameters and initialized ones,
   //but I think functionally they could be considered the same.
   updateCd: (key: keyof composer, value: any, immediate?: boolean) => void
 }
-const ComposerDraftContext = createContext({} as composer_draft_struct);
+const ComposerDraftContext = createContext({} as composer_draft_context_t);
 export default ComposerDraftContext;
