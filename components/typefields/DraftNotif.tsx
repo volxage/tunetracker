e6a6be889b338e369eefad72a426c16373787663
volxage <jhilla@jhilla.org>
2025-07-11 15:23:38 -0400

    Add almost empty FindDifferences function To find differences between drafts and standards

--- components/typefields/DraftNotif.tsx
+++ components/typefields/DraftNotif.tsx
@@ -5,7 +5,7 @@ import {SMarginView, SubBoldText, SubDimText, SubText, Text} from "../../Style";
 import {useContext, useEffect, useState} from "react";
 import TuneDraftContext, {tune_draft_context_t} from "../../contexts/TuneDraftContext";
 import ComposerDraftContext, {composer_draft_context_t} from "../../contexts/ComposerDraftContext";
-import {tune_draft, composer} from "../../types";
+import {tune_draft, composer, standard_composer, standard} from "../../types";
 import {useNavigation} from "@react-navigation/native";
 import {Button} from "../../simple_components/Button";
 import {NewTuneContext} from "../Editor";
@@ -20,8 +20,19 @@ type notification_t = {
     action: Function
   }[]
 }
+type draft_context_t = tune_draft_context_t | composer_draft_context_t;
+function FindDifferences(draftContext: draft_context_t, std: standard | standard_composer){
+  if("cd" in draftContext){
+    //3 cases to target:
+    //1: key exists in local but not in standard
+    //2: key exists in both, but value is different (what does different mean in special cases?)
+    //3: key exists in standard, but not local
+    //Make a list of keys that should exist in both, and translate to standard key
+    //Does this call for version numbers in drafts and tunes/composers?
 
-function ParseNotifications(draftContext: tune_draft_context_t | composer_draft_context_t, navigation: any){
+  }
+}
+function ParseNotifications(draftContext: draft_context_t, navigation: any){
   const notifications = [] as notification_t[];
   if("cd" in draftContext){
     const cd = draftContext.cd;
@@ -54,6 +65,7 @@ function ParseNotifications(draftContext: tune_draft_context_t | composer_draft_
           }
         ]
       });
+    }else{
     }
   }
   return notifications;
