68a95f3c6b87b8852ffbb110b406589f01bd576e
volxage <jhilla@jhilla.org>
2025-07-14 13:41:24 -0400

    Move findDifferences skeleton to dedicated file

---
+++ DraftReducers/utils/findDifferences.ts
@@ -0,0 +1,12 @@
+type draft_context_t = tune_draft_context_t | composer_draft_context_t;
+export function findDifferences(draftContext: draft_context_t, std: standard | standard_composer){
+  if("cd" in draftContext){
+    //3 cases to target:
+    //1: key exists in local but not in standard
+    //2: key exists in both, but value is different (what does different mean in special cases?)
+    //3: key exists in standard, but not local
+    //Make a list of keys that should exist in both, and translate to standard key
+    //Does this call for version numbers in drafts and tunes/composers?
+
+  }
+}
