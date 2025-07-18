b0e1a40acf80565573dbed10ec43a60fecf58f99
volxage <jhilla@jhilla.org>
2025-07-18 15:42:52 -0400

    Add id to ComposerDraftContext

--- contexts/ComposerDraftContext.ts
+++ contexts/ComposerDraftContext.ts
@@ -1,11 +1,13 @@
 import {createContext} from "react"
 import {composer} from "../types";
+import {BSON} from "realm";
 export type composer_draft_context_t = {
   cd: composer,
   setCd: (cd: composer) => void,
   //There's a technical difference between optional parameters and initialized ones,
   //but I think functionally they could be considered the same.
-  updateCd: (key: keyof composer, value: any, immediate?: boolean) => void
+  updateCd: (key: keyof composer, value: any, immediate?: boolean) => void,
+  id?: BSON.ObjectId
 }
 const ComposerDraftContext = createContext({} as composer_draft_context_t);
 export default ComposerDraftContext;
