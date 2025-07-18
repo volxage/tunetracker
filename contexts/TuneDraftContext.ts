b0a7bb6a561e863bcee7832a959a07a35a5f595c
volxage <jhilla@jhilla.org>
2025-07-18 15:41:45 -0400

    Add id to TuneDraft context

--- contexts/TuneDraftContext.ts
+++ contexts/TuneDraftContext.ts
@@ -1,11 +1,13 @@
 import {createContext} from "react"
 import {tune_draft, tune_draft_extras} from "../types";
+import {BSON} from "realm";
 export type tune_draft_context_t = {
   td: tune_draft,
   setTd: (td: tune_draft) => void,
   //There's a technical difference between optional parameters and initialized ones,
   //but I think functionally they could be considered the same.
-  updateTd: (key: keyof (tune_draft & tune_draft_extras), value: any, immediate?: boolean) => void
+  updateTd: (key: keyof (tune_draft & tune_draft_extras), value: any, immediate?: boolean) => void,
+  id?: BSON.ObjectId
 }
 const TuneDraftContext = createContext({} as tune_draft_context_t);
 export default TuneDraftContext;
