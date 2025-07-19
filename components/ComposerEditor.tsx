26fe4b056fa3a2c81fccc4aa089e16b8e5fb88db
volxage <jhilla@jhilla.org>
2025-07-19 12:32:45 -0400

    Add NewComposerContext provider to ComposerEditor

--- components/ComposerEditor.tsx
+++ components/ComposerEditor.tsx
@@ -30,6 +30,7 @@ import {Button} from '../simple_components/Button.tsx';
 import SimilarItemPrompt from './connection-management/SimilarItemPrompt.tsx';
 import UploadRequest from './connection-management/UploadRequest.tsx';
 import ConfirmConectionPrompt from './connection-management/ConfirmConnectionPrompt.tsx';
+import {BSON} from 'realm';
 
 export const NewComposerContext = createContext(false);
 
@@ -75,127 +76,132 @@ export default function ComposerEditor({
     onlineVersion.death = onlineVersion.death ? new Date(onlineVersion.death) : undefined;
   }
   return (
-    <ComposerDraftContext.Provider value={{cd: state["currentDraft"], setCd: () => {},  updateCd: handleSetCurrentComposer, id: selectedComposer?.id}}>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name={"EditorUnwrapped"} >
-          {props => <SafeBgView>
-            <FlatList
-              data={prettyAttrs}
-              renderItem={({item, index, separators}) => (
-                <View>
-                  <TouchableHighlight
-                    key={item[0]}
-                    onShowUnderlay={separators.highlight}
-                    onHideUnderlay={separators.unhighlight}
-                  >
-                    <TypeField
-                      attr={state["currentDraft"][item[0]]}
-                      attrKey={item[0]}
-                      attrName={item[1]}
-                      handleSetCurrentItem={handleSetCurrentComposer}
-                      isComposer={true}
-                    />
-                  </TouchableHighlight>
-                </View>
-              )}
-              ListFooterComponent={
-                <View>
-                  <SubText style={{fontSize: 16, color:'grey', alignSelf: 'center'}}>
-                    Press and hold if you're sure
-                  </SubText>
-                  {
-                    !newComposer && 
-                      <DeleteButton
-                        onLongPress={() => {
-                          realm.write(() => 
-                            realm.delete(selectedComposer)
-                          )
-                          navigation.goBack();
-                        }}>
-                        <ButtonText>DELETE COMPOSER (CAN'T UNDO!)</ButtonText>
-                      </DeleteButton>
-                  }
-                  <View style={{flexDirection: "row"}}>
-                    <View style={{flex: 1}}>
+    <NewComposerContext.Provider value={newComposer}>
+      <ComposerDraftContext.Provider value={{cd: state["currentDraft"], setCd: () => {},  updateCd: handleSetCurrentComposer, id: selectedComposer?.id}}>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name={"EditorUnwrapped"} >
+            {props => <SafeBgView>
+              <FlatList
+                data={prettyAttrs}
+                renderItem={({item, index, separators}) => (
+                  <View>
+                    <TouchableHighlight
+                      key={item[0]}
+                      onShowUnderlay={separators.highlight}
+                      onHideUnderlay={separators.unhighlight}
+                    >
+                      <TypeField
+                        attr={state["currentDraft"][item[0]]}
+                        attrKey={item[0]}
+                        attrName={item[1]}
+                        handleSetCurrentItem={handleSetCurrentComposer}
+                        isComposer={true}
+                      />
+                    </TouchableHighlight>
+                  </View>
+                )}
+                ListFooterComponent={
+                  <View>
+                    <SubText style={{fontSize: 16, color:'grey', alignSelf: 'center'}}>
+                      Press and hold if you're sure
+                    </SubText>
+                    {
+                      !newComposer && 
+                        <DeleteButton
+                          onLongPress={() => {
+                            realm.write(() => 
+                              realm.delete(selectedComposer)
+                            )
+                            navigation.goBack();
+                          }}>
+                          <ButtonText>DELETE COMPOSER (CAN'T UNDO!)</ButtonText>
+                        </DeleteButton>
+                    }
+                    <View style={{flexDirection: "row"}}>
+                      <View style={{flex: 1}}>
 
-                      {
-                      }
-                      {
-                        !newComposer &&
-                          <Button
-                            onPress={() => {
-                              realm.write(() => {
-                                let attr: keyof composer;
-                                for(attr in state["currentDraft"]){
-                                  selectedComposer[attr] = state["currentDraft"][attr]; //as (string & Date | undefined); Why does the model want this type?
-                                }
-                              });
-                              navigation.goBack()
-                            }}
-                            text='Save'
-                          />
-                      }
-                      {
-                        newComposer &&
-                          <Button
-                            onPress={() => {
-                              //database.write(async () => {database.get('composers').create(comp => {
-                              //  (comp as Composer).replace(state["currentDraft"])
-                              //}).then(resultingModel => {
-                              //  console.log(resultingModel);
-                              //})});
-                              realm.write(() => {
-                                realm.create("Composer", state["currentDraft"]);
-                              });
-                              navigation.goBack();
-                              setNewComposer(false);
-                            }}
-                            text='Save'
-                          />
-                      }
+                        {
+                        }
+                        {
+                          !newComposer &&
+                            <Button
+                              onPress={() => {
+                                realm.write(() => {
+                                  let attr: keyof composer;
+                                  for(attr in state["currentDraft"]){
+                                    selectedComposer[attr] = state["currentDraft"][attr]; //as (string & Date | undefined); Why does the model want this type?
+                                  }
+                                });
+                                navigation.goBack()
+                              }}
+                              text='Save'
+                            />
+                        }
+                        {
+                          newComposer &&
+                            <Button
+                              onPress={() => {
+                                //database.write(async () => {database.get('composers').create(comp => {
+                                //  (comp as Composer).replace(state["currentDraft"])
+                                //}).then(resultingModel => {
+                                //  console.log(resultingModel);
+                                //})});
+                                //WHY DOES THIS WORK?
+                                const ctCopy = state["currentDraft"]
+                                ctCopy.id = new BSON.ObjectId()
+                                realm.write(() => {
+                                  realm.create("Composer", state["currentDraft"]);
+                                });
+                                navigation.goBack();
+                                setNewComposer(false);
+                              }}
+                              text='Save'
+                            />
+                        }
 
 
-                    </View>
-                    <View style={{flex: 1}}>
-                      <DeleteButton
-                        onPress={() => {navigation.goBack(); setNewComposer(false);}}
-                      ><ButtonText>Cancel Edit</ButtonText></DeleteButton>
+                      </View>
+                      <View style={{flex: 1}}>
+                        <DeleteButton
+                          onPress={() => {navigation.goBack(); setNewComposer(false);}}
+                        ><ButtonText>Cancel Edit</ButtonText></DeleteButton>
+                      </View>
                     </View>
                   </View>
-                </View>
-              }
-            />
-          </SafeBgView>}
-        </Stack.Screen>
-        <Stack.Screen name={"ComposerImportId"}>
-          {props => 
-            <SafeAreaView style={{flex: 1}}>
-              <ComposerDraftContext.Provider value={{cd: state["currentDraft"], setCd: () => {}, updateCd: handleSetCurrentComposer}}>
-                <Importer
-                  importingComposers={true}
-                  importingId={true}
-                  importFn={function(stand: standard, mini: boolean){
-                    handleSetCurrentComposer("dbId", stand.id)
-                    props.navigation.goBack();
-                  }}/>
-              </ComposerDraftContext.Provider>
-            </SafeAreaView>
-          }
-        </Stack.Screen>
-        <Stack.Screen name="ComposerCompare">
-          {props =>
-            <Compare
-              currentItem={state["currentDraft"]}
-              onlineVersion={(state["currentDraft"].dbId ? OnlineDB.getComposerById(state["currentDraft"].dbId) : null) as standard_composer}
-              handleSetCurrentItem={handleSetCurrentComposer}
-              isComposer={true}
-            />
-          }
-        </Stack.Screen>
-        <Stack.Screen name='SimilarItemPrompt' component={SimilarItemPrompt}/>
-        <Stack.Screen name='UploadRequest' component={UploadRequest}/>
-        <Stack.Screen name='ConfirmConnectionPrompt' component={ConfirmConectionPrompt}/>
-      </Stack.Navigator>
-    </ComposerDraftContext.Provider>
+                }
+              />
+            </SafeBgView>}
+          </Stack.Screen>
+          <Stack.Screen name={"ComposerImportId"}>
+            {props => 
+              <SafeAreaView style={{flex: 1}}>
+                <ComposerDraftContext.Provider value={{cd: state["currentDraft"], setCd: () => {}, updateCd: handleSetCurrentComposer}}>
+                  <Importer
+                    importingComposers={true}
+                    importingId={true}
+                    importFn={function(stand: standard, mini: boolean){
+                      handleSetCurrentComposer("dbId", stand.id)
+                      props.navigation.goBack();
+                    }}/>
+                </ComposerDraftContext.Provider>
+              </SafeAreaView>
+            }
+          </Stack.Screen>
+          <Stack.Screen name="ComposerCompare">
+            {props =>
+              <Compare
+                currentItem={state["currentDraft"]}
+                onlineVersion={(state["currentDraft"].dbId ? OnlineDB.getComposerById(state["currentDraft"].dbId) : null) as standard_composer}
+                handleSetCurrentItem={handleSetCurrentComposer}
+                isComposer={true}
+              />
+            }
+          </Stack.Screen>
+          <Stack.Screen name='SimilarItemPrompt' component={SimilarItemPrompt}/>
+          <Stack.Screen name='UploadRequest' component={UploadRequest}/>
+          <Stack.Screen name='ConfirmConnectionPrompt' component={ConfirmConectionPrompt}/>
+        </Stack.Navigator>
+      </ComposerDraftContext.Provider>
+    </NewComposerContext.Provider>
   );
 }
