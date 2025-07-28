5c983bb348a7647a99a9885acbb80b14298d1570
volxage <jhilla@jhilla.org>
2025-07-28 15:20:14 -0400

    New SetlistBuilder file

---
+++ components/SetlistBuilder.tsx
@@ -0,0 +1,10 @@
+import {SafeAreaView} from "react-native";
+import {BgView} from "../Style";
+
+export default function SetlistBuilder({}:{}){
+  return(
+    <BgView>
+      <SafeAreaView></SafeAreaView>
+    </BgView>
+  )
+}
