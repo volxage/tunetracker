861a236c2d7363a88274f795c4a6a2b3e6841939
volxage <jhilla@jhilla.org>
2026-07-06 21:17:54 -0400

    Remove broken import on index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * @format
  */
 
-import 'react-native-get-random-values'
 import {AppRegistry, DevSettings, NativeModules} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
