c207d650571e910f1383841f4334c26f1519ce0a
volxage <jhilla@jhilla.org>
2026-07-08 12:33:27 -0400

    Add react-native-get-random-values for Realm BSON

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @format
  */
 
+import 'react-native-get-random-values';
 import {AppRegistry, DevSettings, NativeModules} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
